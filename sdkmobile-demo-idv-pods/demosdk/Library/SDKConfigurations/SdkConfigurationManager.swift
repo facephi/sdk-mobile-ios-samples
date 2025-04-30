252cbbfdf41974321dafb970af68f53d35c9472e
Jorge Poveda Pérez <80745302+jpovedap@users.noreply.github.com>
2025-04-30 11:10:39 +0200

    Feat/sm 4878 add idv demo sample (#3)
    * [SM-4878] feat: Added .gitignore to repository
    
    * [SM-4878] release: Created IDV demo sample

---
+++ sdkmobile-demo-idv-pods/demosdk/Library/SDKConfigurations/SdkConfigurationManager.swift
@@ -0,0 +1,23 @@
+//
+//  SdkConfigurationManager.swift
+//  demosdk
+//
+//  Created by Jorge Poveda on 22/5/23.
+//
+
+import Foundation
+
+struct SdkConfigurationManager {
+    static let CUSTOMER_ID = "sdk-idv-ios-example@fphi"
+    static let APIKEY_LICENSING = ""
+    static let LICENSING_URL = URL(string: "licensing_URL")!
+    static let onlineConfiguration = false
+    static let FLOW_ID = "6cdf5827-df3c-4c00-8ce7-02ed8a633424"
+    
+    // swiftlint:disable all
+    static let license = """
+insert_here_your_offline_license_if_needed
+"""
+}
+
+
---
+++ sdkmobile-demo-idv-pods/demosdk/Library/SDKConfigurations/SelphIDConfiguration.swift
@@ -0,0 +1,42 @@
+//
+//  SelphIDConfiguration.swift
+//  demosdk
+//
+//  Created by Jorge Poveda on 22/5/23.
+//
+
+import Foundation
+import selphidComponent
+
+extension SdkConfigurationManager {
+    static var selphIDConfiguration: SelphIDConfigurationData {
+        var configSelphID = SelphIDConfigurationData()
+        
+        let resourcesSelphID: String = {
+            let selphiZipName = "resources-selphid-2-0"
+            return Bundle.main.path(
+                forResource: selphiZipName,
+                ofType: "zip") ?? ""
+        }()
+        
+        configSelphID.debug = false
+        configSelphID.wizardMode = true
+        configSelphID.showResultAfterCapture = false
+        configSelphID.showTutorial = false
+        configSelphID.scanMode = SelphIDScanMode.MODE_SEARCH
+        configSelphID.specificData = "ES|<ALL>"
+        configSelphID.fullscreen = true
+        configSelphID.tokenImageQuality = 0.5
+        configSelphID.documentType = SelphIDDocumentType.ID_CARD
+        configSelphID.documentSide = SelphIDDocumentSide.FRONT
+        configSelphID.timeout = SelphIDTimeout.LONG
+        configSelphID.generateRawImages = true
+        configSelphID.translationsContent = ""
+        configSelphID.viewsContent = ""
+        configSelphID.documentModels = ""
+        configSelphID.resourcesPath = resourcesSelphID
+        configSelphID.showDiagnostic = true
+        
+        return configSelphID
+    }
+}
